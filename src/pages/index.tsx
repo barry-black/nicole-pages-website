e78401645a820bf15bdfb921c4222309495f2656
Thomas <barryclashofclan@gmail.com>
2025-03-16 10:34:29 +0100

    Refactoring of debug page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,63 +8,98 @@ export default function Home() {
     <>
       <Seo title="Nicole Pagès Thérapie" description="Description..." />
 
-      <div className="bg-gray-400 p-4">
-        <div className="flex flex gap-4 p-10">
-          <Spinner size="small"></Spinner>
-          <Spinner size="medium" variant="secondary"></Spinner>
-          <Spinner size="large"></Spinner>
+      <div className="bg-gray-400 p-10">
+        {/* Spinner */}
+        <div className="flex items-start gap-10">
+          <div className="space-y-2">
+            <Typography variant="m-24" weight="medium">
+              Spinner
+            </Typography>
+            <div className="flex items-center gap-10 p-5 border border-white rounded">
+              <Spinner size="small"></Spinner>
+              <Spinner size="medium" variant="secondary"></Spinner>
+              <Spinner size="large"></Spinner>
+            </div>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-4 p-10">
-          <Button variant="topMenu">les soins</Button>
-          <Button variant="info">+ d&apos;info</Button>
-          <Button variant="callAction">Prendre rendez-vous</Button>
-          <Button variant="disabled">Prendre rendez-vous</Button>
-          <Button variant="ico" icon="/assets/svg/papillon_blanc.png">
-            Nicole Pagès
-          </Button>
+        {/* Button */}
+        <div className="flex items-start gap-10">
+          <div className="space-y-2">
+            <Typography variant="m-24" weight="medium">
+              Buttons
+            </Typography>
+            <div className="flex flex-col gap-5 p-5 border border-white rounded">
+              <Button variant="topMenu">les soins</Button>
+              <Button variant="info">+ d&apos;info</Button>
+              <Button variant="callAction">Prendre rendez-vous</Button>
+              <Button variant="disabled">Prendre rendez-vous</Button>
+              <Button variant="ico" icon="/assets/svg/papillon_blanc.png">
+                Nicole Pagès
+              </Button>
+              <Button isLoading variant="topMenu">
+                les soins
+              </Button>
+              <Button isLoading variant="info">
+                + d&apos;info
+              </Button>
+              <Button isLoading variant="callAction">
+                Prendre rendez-vous
+              </Button>
+            </div>
+          </div>
         </div>
 
-        <div className="flex flex-col gap-4 p-10">
-          <Button isLoading variant="topMenu">les soins</Button>
-          <Button isLoading variant="info">+ d&apos;info</Button>
-          <Button isLoading variant="callAction">Prendre rendez-vous</Button>
-        </div>
-
-        <div className="gap-4 p-10">
-          <Typography variant="ds-40" theme="ocean-blue" component="h1">
-            Text
-          </Typography>
-          <Typography variant="ds-50" theme="white" component="h1">
-            Text
-          </Typography>
-          <Typography variant="ds-62-shadow" theme="ocean-blue" component="h1">
-            Text
-          </Typography>
-          <Typography variant="m-18" theme="black" component="div">
-            Text
-          </Typography>
-          <Typography variant="m-18-italic" theme="pale-blue" component="div">
-            Text
-          </Typography>
-          <Typography variant="m-19" theme="ocean-blue" component="div">
-            Text
-          </Typography>
-          <Typography variant="m-20" theme="black" component="div">
-            Text
-          </Typography>
-          <Typography variant="m-24" theme="pale-blue" component="div">
-            Text
-          </Typography>
-          <Typography variant="m-36" theme="ocean-blue" component="div">
-            Text
-          </Typography>
-          <Typography variant="m-40" theme="black" component="div">
-            Text
-          </Typography>
-          <Typography variant="m-88-shadow" theme="white" component="div">
-            Text
-          </Typography>
+        {/* Typography */}
+        <div className="flex items-start gap-10">
+          <div className="space-y-2">
+            <Typography variant="m-24" weight="medium">
+              Typography
+            </Typography>
+            <div className="flex flex-col gap-5 p-5 border border-white rounded">
+              <Typography variant="ds-40" theme="ocean-blue" component="h1">
+                Text
+              </Typography>
+              <Typography variant="ds-50" theme="white" component="h1">
+                Text
+              </Typography>
+              <Typography
+                variant="ds-62-shadow"
+                theme="ocean-blue"
+                component="h1"
+              >
+                Text
+              </Typography>
+              <Typography variant="m-18" theme="black" component="div">
+                Text
+              </Typography>
+              <Typography
+                variant="m-18-italic"
+                theme="pale-blue"
+                component="div"
+              >
+                Text
+              </Typography>
+              <Typography variant="m-19" theme="ocean-blue" component="div">
+                Text
+              </Typography>
+              <Typography variant="m-20" theme="black" component="div">
+                Text
+              </Typography>
+              <Typography variant="m-24" theme="pale-blue" component="div">
+                Text
+              </Typography>
+              <Typography variant="m-36" theme="ocean-blue" component="div">
+                Text
+              </Typography>
+              <Typography variant="m-40" theme="black" component="div">
+                Text
+              </Typography>
+              <Typography variant="m-88-shadow" theme="white" component="div">
+                Text
+              </Typography>
+            </div>
+          </div>
         </div>
       </div>
     </>
