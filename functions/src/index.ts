6a6cc5ef41b3bbc295c313938718816ad5c05309
Thomas <barryclashofclan@gmail.com>
2025-06-02 22:41:40 +0100

    Deploy sendMail in firebase

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,43 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import nodemailer from "nodemailer";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: "nicolepages.therapie@gmail.com",
+        pass: functions.config().gmail.pass,
+    },
+});
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const sendMail = functions
+    .region("europe-west1")
+    .runWith({ memory: "256MB", timeoutSeconds: 10 })
+    .https.onCall(
+        async (
+            request: any
+        ) => {
+            const { prenom, nom, email, telephone, message } = request.data;
+
+            const mailOptions = {
+                from: "nicolepages.therapie@gmail.com",
+                to: "nicolepages.therapie@gmail.com",
+                subject: "Prise de rendez-vous via le site",
+                text: `Bonjour Nicole,\n\nVous avez reçu un message depuis le formulaire :\n\nNom : ${prenom} ${nom}\nEmail : ${email}\nTéléphone : ${telephone}\n\nMessage :\n${message}`,
+            };
+
+            try {
+                await transporter.sendMail(mailOptions);
+                console.log("Email envoyé avec succès !");
+                return { success: true };
+            } catch (error) {
+                console.error("Erreur d’envoi d’email :", error);
+                throw new functions.https.HttpsError(
+                    "internal",
+                    "Erreur lors de l'envoi de l’e-mail."
+                );
+            }
+        }
+    );
