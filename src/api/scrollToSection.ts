eeec075e2b30d1059c5c67d5075b11dbdbb1d0c4
Thomas <barryclashofclan@gmail.com>
2025-06-04 20:03:38 +0100

    Fix scroll with animation

--- src/api/scrollToSection.ts
+++ src/api/scrollToSection.ts
@@ -1,10 +1,20 @@
 export const scrollToSection = (id: string) => {
-    const section = document.getElementById(id);
-    const header = document.querySelector("header");
+    const scrollTo = () => {
+        const section = document.getElementById(id);
+        const header = document.querySelector("header");
 
-    if (section && header) {
-        const yOffset = -header.offsetHeight;
-        const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
-        window.scrollTo({ top: y, behavior: "smooth" });
-    }
+        if (section && header) {
+            const yOffset = -header.offsetHeight;
+            const y = section.getBoundingClientRect().top + window.pageYOffset + yOffset;
+            window.scrollTo({ top: y, behavior: "smooth" });
+        }
+    };
+
+    // Scroll immédiatement
+    scrollTo();
+
+    // Re-scroll après animation (prévient bugs liés à whileInView)
+    setTimeout(() => {
+        scrollTo();
+    }, 600);
 };
